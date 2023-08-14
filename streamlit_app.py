14a789a2df6e7d6228a9cd84dd768b542f391d13
KhalfaniNovianHabibi <habibialfan26@gmail.com>
2023-08-15 05:51:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#Fungsi
+def modifikasi_data():
+    # Umur
+    df['USIA'] = pd.cut(
+        x=df['USIA'],
+        bins=[0,17,64,np.inf],
+        labels=['Anak-Anak','Dewasa','Lansia']
+        )
+
 # Streamlit
 with st.sidebar:
     st.sidebar.title("Source Code")
@@ -10,12 +19,13 @@ with st.sidebar:
         <https://github.com/TetukoAnglingKusumo/STREAMLIT-APP>
         """
     )
-    uploaded_file = st.file_uploader("Upload File XLSX", type="xlsx")
+    uploaded_file = st.file_uploader("Unggah File XLSX", type="xlsx")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file, skiprows=[0,1])
-    st.title("Streamlit Apps")
+    st.title("Aplikasi Streamlit")
     st.markdown("## Dataset")
     st.write('Jumlah Data :',len(df))
-    with st.expander("Expand **Raw Data**"):
+    with st.expander("Data Asli"):
+        modifikasi_data()
         st.dataframe(df)
